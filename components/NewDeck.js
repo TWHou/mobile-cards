91210be4eff24782da6df2a6bab97e4e1458887d
Christina Hsieh <hsiehkunlun@hotmail.com>
2017-09-29 22:45:45 -0400

    feat: setup new deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, Button } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+
+import { addDeck } from '../utils/api';
 
 export default class NewDeck extends Component {
 
@@ -7,10 +10,39 @@ export default class NewDeck extends Component {
     title: 'Add New Deck',
   };
 
+  state = {
+    name: ''
+  }
+
+  onSubmit = () => {
+    // Check for duplicate entry
+    const deck = {
+      [this.state.name]: {
+        title: this.state.name,
+        questions: []
+      }
+    };
+    addDeck(deck).then(() => {
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({ routeName: 'Home'})]
+      });
+      this.props.navigation.dispatch(resetAction);
+    });
+  }
+
   render() {
     return (
       <View>
         <Text> New Deck View </Text>
+        <TextInput
+          onChangeText={(name) => this.setState({name})}
+          placeholder="Name of Deck"
+        />
+        <Button
+          title="Submit"
+          onPress={this.onSubmit}
+        />
       </View>
     );
   }
